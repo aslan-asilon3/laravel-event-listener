c19cea7703e9c0b7114f05b559009945e030145b
aslan-asilon3 <sulaslansetiawan3@gmail.com>
2022-09-27 16:18:23 -0700

    update send email with try catch

--- app/Http/Controllers/SendMailController.php
+++ app/Http/Controllers/SendMailController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use Exception;
 use App\Events\errorLog;
+use Illuminate\Support\Facades\Log;
 
 class SendMailController extends Controller
 {
@@ -14,16 +15,38 @@ class SendMailController extends Controller
     {
 
         return view('sendmail');
-    
+
     }
 
-    public function send(Request $details)
-    {
-             throw new Exception("TES BUAT ERROR KIRIM KE LOG");
 
-            $data = $details->all();
-            Mail::to('sulaslansetiawan3@gmail.com')->send(new \App\Mail\SendEmail($details));
-            alert()->success('SuccessAlert','Email Berhasil Dikirim  Yaa Aslan.');
+
+    // TES pke try catch
+        public function send(Request $details)
+    {
+
+                try{
+
+                alert()->success('SuccessAlert','Email Berhasil Dikirim  Yaa Aslan.');
+                Log::info("Email Berhasil Dikirim  Yaa Aslan.");
+
+            }catch(\Throwable $th){
+                // event(new errorLog($th->getMessage()));
+                $data = $details->all();
+                Mail::to('sulaslansetiawan3@gmail.com')->send(new \App\Mail\SendEmail($details));
+
+            }
             return view('sendmail');
     }
+
+
+
+    // public function send(Request $details)
+    // {
+    //         //  throw new Exception("TES BUAT ERROR KIRIM KE LOG");
+
+    //         $data = $details->all();
+    //         Mail::to('sulaslansetiawan3@gmail.com')->send(new \App\Mail\SendEmail($details));
+    //         alert()->success('SuccessAlert','Email Berhasil Dikirim  Yaa Aslan.');
+    //         return view('sendmail');
+    // }
 }
